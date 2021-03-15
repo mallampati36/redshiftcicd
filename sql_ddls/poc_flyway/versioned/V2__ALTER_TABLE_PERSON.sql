02a4679409b1d8470180b17bb907177b153fa139
mallampati36 <70365285+mallampati36@users.noreply.github.com>
2021-03-15 10:38:00 +0530

    Delete V2__ALTER_TABLE_PERSON.sql

--- sql_ddls/poc_flyway/versioned/V2__ALTER_TABLE_PERSON.sql
+++
@@ -1,5 +0,0 @@
---Alter table PERSON to rename column and add new column
-alter table poc_flyway.PERSON
-rename column NAME to FIRST_NAME;
-alter table poc_flyway.PERSON
-ADD column LAST_NAME varchar(100);
